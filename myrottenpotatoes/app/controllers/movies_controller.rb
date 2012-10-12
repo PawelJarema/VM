a69b35f78209a0ae93a2d3b507300e1a8d81ae6c
Ubuntu <ubuntu@ip-10-160-39-87.us-west-1.compute.internal>
2012-10-12 16:01:59 +0000

    There's now a edit method. Buttons in main view to edit and delete

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -10,10 +10,33 @@ class MoviesController < ApplicationController
     # like this
   end
 
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
   def show
     id = params[:id]         # retrieve movie ID from URI route
     @movie = Movie.find(id)  # lookup movie by ID
     # the command above will render /app/views/movies/show.html.haml by default
   end
 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
 end
